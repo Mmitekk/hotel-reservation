808f5e5fdd506af524115efe43005c2563662e06
Mmitekk <mitish.n@yandex.ru>
2026-09-05 13:07:56 +0500

    feat: checkin hover readable, privacy consent in booking form v1.12.11

--- src/Plugin/Block/BookingFormBlock.php
+++ src/Plugin/Block/BookingFormBlock.php
@@ -312,6 +312,13 @@ class BookingFormBlock extends BlockBase {
       $html .= '</div>';
     }
 
+    $html .= '<div class="hr-booking-policy">';
+    $html .= 'Нажимая на кнопку «Отправить», я принимаю условия ';
+    $html .= '<a href="https://xn----7sbckjabji9f3df.xn--p1ai/%D1%81%D0%BE%D0%B3%D0%BB%D0%B0%D1%81%D0%B8%D0%B5-%D0%BD%D0%B0-%D0%BE%D0%B1%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D0%BA%D1%83-%D0%BF%D0%B5%D1%80%D1%81%D0%BE%D0%BD%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D1%85-%D0%B4%D0%B0%D0%BD%D0%BD%D1%8B%D1%85">согласия на обработку персональных данных</a>, ';
+    $html .= '<a href="https://xn----7sbckjabji9f3df.xn--p1ai/%D0%BF%D0%BE%D0%BB%D0%B8%D1%82%D0%B8%D0%BA%D0%B0-%D0%BA%D0%BE%D0%BD%D1%84%D0%B8%D0%B4%D0%B5%D0%BD%D1%86%D0%B8%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D0%B8"> политики конфиденциальности</a> и ';
+    $html .= '<a href="https://xn----7sbckjabji9f3df.xn--p1ai/%D0%BF%D1%83%D0%B1%D0%BB%D0%B8%D1%87%D0%BD%D0%B0%D1%8F-%D0%BE%D1%84%D0%B5%D1%80%D1%82%D0%B0">оферты</a>.';
+    $html .= '</div>';
+
     $html .= '<button type="button" class="hr-btn hr-btn--primary hr-book-btn" data-btn-text="' . Html::escape($button_text) . '">' . Html::escape($button_text) . '</button>';
     $html .= '</div>';
 
